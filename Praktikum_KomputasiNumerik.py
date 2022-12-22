b8a1247286b38c26acdf851516f8700c4d07b217
NandaVahindra <nandavahindra@gmail.com>
2022-12-22 11:04:59 +0700

    Update readme

--- Praktikum_KomputasiNumerik.py
+++ Praktikum_KomputasiNumerik.py
@@ -15,7 +15,7 @@ def trapezoidal(x, y, n):
         s += f(x + i*h)
     return h * s
 
-# Menghitung nilai integral dengan metode Trapezoidal dan Integrasi Romberg
+# Menghitung nilai integral dengan metode Trapezoidal dan Integrasi Romberg dan menampilkan hasilnya
 x = 0
 y = 1
 n = 4
@@ -24,7 +24,7 @@ romberg = integrate.romberg(interg, x, y, show = True)
 print("Hasil perhitungan dengan metode Trapezoidal:", trapezoidal)
 print("Hasil perhitungan dengan metode Integrasi Romberg:", romberg)
 
-# Error Relative
+# Mencari error relatif dan menampilkan hasilnya
 result = 0.3333333333333333  
 print("Trapezoidal Er :", abs(result - trapezoidal) / abs(result))
 print("Romberg Er :", abs(result - romberg) / abs(result))  
