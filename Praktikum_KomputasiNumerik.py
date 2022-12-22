43a667182135b9a6326ed4e29c8f83b1e90e2086
NandaVahindra <nandavahindra@gmail.com>
2022-12-22 11:00:55 +0700

    Add minor changes

--- Praktikum_KomputasiNumerik.py
+++ Praktikum_KomputasiNumerik.py
@@ -1,13 +1,13 @@
 import numpy as np
 from scipy import integrate 
 
-# Fungsi yang akan diintegrasikan
+#Masukkan Fungsi Integrasi
 def f(x):
     return x**2
     
 interg = lambda x: f(x)
 
-# Metode Trapezoidal
+# Fungsi untuk menghitung integrasi dengan Metode Trapezoidal
 def trapezoidal(x, y, n):
     h = (y - x) / n
     s = 0.5 * (f(x) + f(y))
@@ -20,15 +20,11 @@ x = 0
 y = 1
 n = 4
 trapezoidal = trapezoidal(x, y, n)
-
-# Metode Integrasi Romberg dengan scipy.integrate.romberg() 
 romberg = integrate.romberg(interg, x, y, show = True) 
 print("Hasil perhitungan dengan metode Trapezoidal:", trapezoidal)
 print("Hasil perhitungan dengan metode Integrasi Romberg:", romberg)
 
-# hasil sebenarnya dari integral x^2 dari 0 sampai 1
-result = 0.3333333333333333  
-
 # Error Relative
+result = 0.3333333333333333  
 print("Trapezoidal Er :", abs(result - trapezoidal) / abs(result))
 print("Romberg Er :", abs(result - romberg) / abs(result))  
